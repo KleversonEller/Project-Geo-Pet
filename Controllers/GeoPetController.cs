dbe9686ba71fe3e011d1cc2a88a993e0dd1d8136
Kleverson Eller <kleverson.a16@gmail.com>
2023-06-03 06:01:13 -0300

    feat: :sparkles: Criando o controller da aplicação

---
+++ Controllers/GeoPetController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using project_geopet.Models;
+using project_geopet.Data;
+using project_geopet.Request;
+
+namespace project_geopet.Controllers;
+
+[ApiController]
+[Route(template:"[Controller]")]
+
+public class GeoPetController : ControllerBase
+{
+    [HttpPost]
+    [Route("CreateCaringPerson")]
+    public async Task<IActionResult> Post(
+        [FromServices] GeoPetContext context,
+        [FromBody] CaringPersonRequest request)
+    {
+        if(!ModelState.IsValid) return BadRequest();
+
+        var caringperson = new CaringPerson
+        {
+            CaringPersonId = Guid.NewGuid(),
+            Cep = request.Cep,
+            Email = request.Email,
+            Name = request.Name,
+            Password = request.Password,
+            CreatedAt = DateTime.Now,
+            UpdatedAt = DateTime.Now
+        };
+        try
+        {
+            await context.CaringPersons.AddAsync(caringperson);
+            await context.SaveChangesAsync();
+            return StatusCode(201, "Caregiver created successfully");
+        }
+        catch (Exception)
+        {
+            return BadRequest();
+        }
+    }
+}
