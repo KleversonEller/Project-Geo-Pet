bd667c81dc0beab0538867f0ff723485af52256f
Kleverson Eller <kleverson.a16@gmail.com>
2023-06-04 18:44:01 -0300

    fix: :bug: Corrigindo relacionamento das tabelas

--- Models/CaringPerson.cs
+++ Models/CaringPerson.cs
@@ -2,13 +2,12 @@ namespace project_geopet.Models;
 
 public class CaringPerson
 {
-    public Guid CaringPersonId {get; set;}
-    public string Name {get; set;}
-    public string Email {get; set;}
-    public int Cep {get; set;}
-    public string Password {get; set;}
+    public Guid Id {get; set;}
+    public string Name {get; set;}=string.Empty;
+    public string Email {get; set;}=string.Empty;
+    public string Cep {get; set;}=string.Empty;
+    public string Password {get; set;}=string.Empty;
     public DateTime? CreatedAt {get; set;}
     public DateTime? UpdatedAt {get; set;}
-
-    public ICollection<Pet>? Pets {get; set;}
+    public virtual List<Pet> Pets {get; set;}
 }
--- Models/Pet.cs
+++ Models/Pet.cs
@@ -1,13 +1,17 @@
+using System.Text.Json.Serialization;
+
 namespace project_geopet.Models;
 
 public class Pet
 {
-    public Guid PetId {get; set;}
-    public string Name {get; set;}
+    public Guid Id {get; set;}
+    public string Name {get; set;}=string.Empty;
     public int Age {get; set;}
-    public string Carrying {get; set;}
-    public string Race {get; set;}
+    public string Carrying {get; set;}=string.Empty;
+    public string Race {get; set;}=string.Empty;
     public DateTime? CreatedAt {get; set;}
     public DateTime? UpdatedAt {get; set;}
-    public CaringPerson? CaringPerson {get; set;}
+    [JsonIgnore]
+    public virtual CaringPerson CaringPerson {get; set;}
+    public Guid CaringPersonId {get; set;}
 }
